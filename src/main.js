b324fb4ff2aa1feb085b0104cf14ac0f295ff077
halim.dinckalmis <halim.dinckalmis@n2mobil.com.tr>
2024-03-14 00:08:08 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
-// import { createApp } from 'vue'
-import { createApp } from '/node_modules/vue/dist/vue.esm-browser.js';
+import { createApp } from 'vue'
 
 import './assets/main.css'
 import App from './App.vue'
